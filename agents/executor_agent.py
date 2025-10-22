590371491bf392ae2fc4000c8ca81130df207842
robert.sirbu <rober.sirbu@heavensolutions.com>
2025-10-22 13:45:08 +0300

    executor update

--- agents/executor_agent.py
+++ agents/executor_agent.py
@@ -9,17 +9,21 @@ class ExecutorAgent:
         self.execution_driver = execution_driver
         self.open_ai_agent = openai_agent
         self.YOLO_detector = WidgetDetector()
-    open_ai_prompt = """
-    You are an Executor Agent that will be responsible of doing and completing actions that are provided in the plan.
-    You have to be simple and concise regarding the future prompts that will be provided in order to complete the actions.
-    You do not plan sequence of  actions or refletc/verify the correctitude of the actions.
-    Constraints:
-    - Respond only with valid outputs expected for current action.
-    - Do not explain or repeat prompts
-    - Do not explain outputs
-    """
+        system_prompt = """
+        You are an Executor Agent that will be responsible of doing and completing actions that are provided in the plan.
+        You have to be simple and concise regarding the future prompts that will be provided in order to complete the actions.
+        You do not plan sequence of  actions or refletc/verify the correctitude of the actions.
+        Constraints:
+        - Respond only with valid outputs expected for current action.
+        - Do not explain or repeat prompts
+        - Do not explain outputs
+        """
+        self.open_ai_agent.send_message(system_prompt)
 
     def detect_ui_using_YOLO(self):
         full_screenshot=self.execution_driver.screenshot()
         self.YOLO_detector.predict(full_screenshot)
         image_with_bbox=self.YOLO_detector.attach_bounding_boxes()
+        detect_ui_prompt = """
+        
+        """
